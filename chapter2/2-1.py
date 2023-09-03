9ccea21085aa702d62a89c0c4a1ff2cd5021d204
hyunfunfun <hyunfunfun@naver.com>
2023-09-03 22:24:46 +0900

    2-1

---
+++ chapter2/2-1.py
@@ -0,0 +1,26 @@
+#변수
+a=12.2
+print(type(a))
+print(a)
+
+#사칙연산
+a=3
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)#나누기
+print(a//b)#몫
+print(a**b)#제곱
+print(a%b)#나머지
+
+#문자열
+a="Life is too short, \nYou need python"
+print(type(a))
+print(a)
+head="Python"
+tail=" is Fun"
+print(head+tail)
+print(head*3)
+print(len(head))
+print(head[3])
