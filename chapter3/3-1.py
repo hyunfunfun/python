9e36757e5ed2dd694a4af77a050e7b002a7ff768
hyunfunfun(gram) <hyunfunfun@naver.com>
2023-09-05 16:26:23 +0900

    3-1 2

--- chapter3/3-1.py
+++ chapter3/3-1.py
@@ -4,3 +4,19 @@ if money :
     print('taxi')
 else:
     print('walk')
+
+a=3
+if a==1:
+    print(1)
+elif a==3:
+    print(3)
+else:
+    print('not')
+
+#while문
+treehit=0
+while treehit<10:
+    treehit+=1
+    print('나무 %d번 찍음'% treehit)
+    if treehit==10:
+        print('나무 쓰러짐')
